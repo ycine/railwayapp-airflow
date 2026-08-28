da143612e87376c5244de352cec45515d5d8e5ed
ycine <marscink@gmail.com>
2026-08-28 18:51:55 +0200

    updated data_check_dag1.py

--- dags/data_check_dag1.py
+++ dags/data_check_dag1.py
@@ -2,14 +2,18 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
+def check():
+    print("Running data quality check+++====+++++")
+    # tutaj wstaw właściwą logikę sprawdzania danych
+    # np. zapytanie do bazy, walidacja liczby wierszy, itp.
+
 with DAG(
     dag_id="data_quality_check",
     start_date=datetime(2024, 1, 1),
-    schedule="@once",   # tylko to, bez schedule_interval
+    schedule="@once",
     catchup=False,
 ) as dag:
     check_task = PythonOperator(
         task_id="check",
         python_callable=check,
-        # nie trzeba przekazywać dag=dag przy użyciu context managera "with DAG(...) as dag"
     )
