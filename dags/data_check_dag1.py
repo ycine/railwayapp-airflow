063c0f9a6cfff2f5f1ffa799dfc9dde9d7773b44
ycine <marscink@gmail.com>
2026-08-28 18:30:14 +0200

    updated data_check_dag1.py

--- dags/data_check_dag1.py
+++ dags/data_check_dag1.py
@@ -1,20 +1,15 @@
-from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-
-
-def check():
-    print("DATA QUALITY CHECK - działa!")
-
+from datetime import datetime
 
 with DAG(
     dag_id="data_quality_check",
     start_date=datetime(2024, 1, 1),
-    schedule="@once",   # ⭐ Airflow 3.x używa tylko `schedule`
+    schedule="@once",   # tylko to, bez schedule_interval
     catchup=False,
 ) as dag:
-
     check_task = PythonOperator(
         task_id="check",
         python_callable=check,
+        # nie trzeba przekazywać dag=dag przy użyciu context managera "with DAG(...) as dag"
     )
