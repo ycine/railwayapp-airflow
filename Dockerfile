d54e5db62aec98742262ae4c9439733273b281a5
ycine <marscink@gmail.com>
2026-08-28 19:09:18 +0200

    updated Dockerfile and data_check_dag1.py

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,8 @@ RUN pip install --no-cache-dir \
       --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt" \
       -r /requirements.txt
 
+RUN pip install apache-airflow-providers-postgres
+
 
 COPY dags /opt/airflow/dags
 COPY --chmod=0755 docker-entrypoint.sh /opt/airflow/railway-entrypoint.sh
--- dags/data_check_dag1.py
+++ dags/data_check_dag1.py
@@ -1,11 +1,19 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from datetime import datetime
 
 def check():
     print("Running data quality check+++====+++++")
     # tutaj wstaw właściwą logikę sprawdzania danych
     # np. zapytanie do bazy, walidacja liczby wierszy, itp.
+
+def fetch_measurements():
+    hook = PostgresHook(postgres_conn_id="Api_hoy_db")  # podmień na swój connection_id z Airflow
+    rows = hook.get_records("SELECT * FROM measurements LIMIT 5;")
+    for row in rows:
+        print(row)
+    return rows
 
 with DAG(
     dag_id="data_quality_check",
@@ -17,3 +25,10 @@ with DAG(
         task_id="check",
         python_callable=check,
     )
+
+    fetch_task = PythonOperator(
+        task_id="fetch_measurements",
+        python_callable=fetch_measurements,
+    )
+
+    check_task >> fetch_task
